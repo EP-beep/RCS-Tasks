96d424d76ab13dd885bae033e141e5fee3f4b27f
Elvis <elviisp97@gmail.com>
2021-02-09 23:10:46 +0200

    Program for tourists

---
+++ End Project/databaseconnection/Configs.java
@@ -0,0 +1,10 @@
+package databaseconnection;
+
+public class Configs {
+	
+	protected static String dbhost = "localhost";
+	protected static String dbport = "3306";
+	protected static String dbuser = "root";
+	protected static String dbpass = "databasepassword";
+	protected static String dbname = "password";
+}
