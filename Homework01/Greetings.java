0a811697693c22099a303687cd3e42a7c7dd0ac0
Elvis <elviisp97@gmail.com>
2021-02-09 23:11:39 +0200

    First Homework

---
+++ Homework01/Greetings.java
@@ -0,0 +1,9 @@
+public class Greetings {
+
+	public static void main(String[] args) {
+		String Name = "Elvis";
+		int Age = (23);
+		System.out.println("Hello, my name is " + Name + ". I'm " + Age + " years old!");
+	}
+
+}
