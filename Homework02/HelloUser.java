edc3eabb0a6abb2f0f5364b24cef44f779fc2fbd
Elvis <elviisp97@gmail.com>
2021-02-09 23:12:16 +0200

    Second Homework

--- Homework02/HelloUser.java
+++ Homework02/HelloUser.java
@@ -5,20 +5,20 @@ public class HelloUser {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input;
-		System.out.print("Lūdzu ievadiet Vārdu un Uzvārdu ar atstarpi: ");
+		System.out.print("Please enter your name and your last name with space: ");
 		input = sc.nextLine();
 		sc.close();
 		
-		int atstarpe;
-		atstarpe = input.indexOf(' ');
-		String vards;
-		vards = input.substring(0, atstarpe);
-		String uzvards;
-		uzvards = input.substring(atstarpe + 1);
+		int space;
+		space = input.indexOf(' ');
+		String name;
+		name = input.substring(0, space);
+		String lastname;
+		lastname = input.substring(space + 1);
 		
-		vards = vards.substring(0, 1).toUpperCase() + vards.substring(1).toLowerCase();
-		System.out.println("'" + vards + "'");
-		System.out.println("'" + uzvards.toUpperCase() + "'");
+		name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+		System.out.println("'" + name + "'");
+		System.out.println("'" + lastname.toUpperCase() + "'");
 	}
 
 }
